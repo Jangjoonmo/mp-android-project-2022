bc225ba13dae919207623982968b070b6c13b93c
cuttingworms <21storeo@gmail.com>
2022-04-14 01:55:46 +0900

    Add Teacher Student Management Layout

---
+++ app/src/main/java/com/example/parentsletterproject/activity/TeacherStudentManagementActivity.java
@@ -0,0 +1,17 @@
+package com.example.parentsletterproject.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.parentsletterproject.R;
+
+public class TeacherStudentManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_student_management);
+    }
+
+}
