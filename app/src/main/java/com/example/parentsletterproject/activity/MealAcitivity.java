d725dd8d4ffe7a122539ccaeaa2302b1ba12edbf
Suah-Cho <sacho0929@gmail.com>
2022-05-15 16:44:55 +0900

    meal layout&activity

---
+++ app/src/main/java/com/example/parentsletterproject/activity/MealAcitivity.java
@@ -0,0 +1,7 @@
+package com.example.parentsletterproject.activity;
+
+public class MealAcitivity {
+    // hopen.neis.go.kr/hub/mealServiceDietInfo?KEY="c0dadaab5ff14ca48e48aa850bb3652a"&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7751096
+
+
+}
