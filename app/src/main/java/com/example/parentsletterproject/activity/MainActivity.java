e81115685fa5c6902002aa6de4a131a9f71f40a4
Suah-Cho <sacho0929@gmail.com>
2022-05-17 17:40:41 +0900

    parsing nice api for xml

--- app/src/main/java/com/example/parentsletterproject/activity/MainActivity.java
+++ app/src/main/java/com/example/parentsletterproject/activity/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.parentsletterproject.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.parentsletterproject.R;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +32,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c0dadaab5ff14ca48e48aa850bb3652a&Type=xml&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7751096";
+        OpenAPI dust = new OpenAPI(url);
+        dust.execute();
+
         Button button = findViewById(R.id.button);
         button.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), TeacherClassManagementActivity.class);
             startActivity(intent);
         });
+    }
+
+
+    public class OpenAPI extends AsyncTask<Void, Void, String> {
+
+        private String url;
+
+        public OpenAPI(String url) {
+
+            this.url = url;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            // parsing할 url 지정(API 키 포함해서)
+
+            DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = null;
+            try {
+                dBuilder = dbFactoty.newDocumentBuilder();
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            }
+            Document doc = null;
+            try {
+                doc = dBuilder.parse(url);
+            } catch (IOException | SAXException e) {
+                e.printStackTrace();
+            }
+
+            // root tag
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName()); // Root element: result
+
+            // 파싱할 tag
+            NodeList nList = doc.getElementsByTagName("row");
+
+            for(int temp = 0; temp < nList.getLength(); temp++){
+                Node nNode = nList.item(temp);
+                if(nNode.getNodeType() == Node.ELEMENT_NODE){
+
+                    Element eElement = (Element) nNode;
+                    Log.d("OPEN_API","data  : " + getTagValue("MLSV_YMD", eElement));
+                    Log.d("OPEN_API","급식 메뉴  : " + getTagValue("DDISH_NM", eElement));
+                    //Log.d("OPEN_API","초미세먼지 : " + getTagValue("pm25Value", eElement));
+                }	// for end
+            }	// if end
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String str) {
+            super.onPostExecute(str);
+        }
+    }
+
+    private String getTagValue(String tag, Element eElement) {
+        NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
+        Node nValue = (Node) nlList.item(0);
+        if(nValue == null)
+            return null;
+        return nValue.getNodeValue();
     }
 }
