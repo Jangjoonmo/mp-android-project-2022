8fd0ba9e59c1ab2a0dd22a44a847cddf0e65876a
Jangjoonmo <99167099+Jangjoonmo@users.noreply.github.com>
2022-06-06 05:10:14 +0900

    edit LoginActivity,RegisterActivity

--- app/src/main/java/com/example/parentsletterproject/activity/MainActivity.java
+++ app/src/main/java/com/example/parentsletterproject/activity/MainActivity.java
@@ -31,14 +31,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c0dadaab5ff14ca48e48aa850bb3652a&Type=xml&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7751096";
-        OpenAPI dust = new OpenAPI(url);
-        dust.execute();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
 
-        Button button = findViewById(R.id.button2);
-        button.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), TeacherClassManagementActivity.class);
-            startActivity(intent);
-        });
+//        String url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c0dadaab5ff14ca48e48aa850bb3652a&Type=xml&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7751096";
+//        OpenAPI dust = new OpenAPI(url);
+//        dust.execute();
+//
+//        Button button = findViewById(R.id.button2);
+//        button.setOnClickListener(view -> {
+//            Intent intent = new Intent(getApplicationContext(), TeacherClassManagementActivity.class);
+//            startActivity(intent);
+//        });
     }
 }
