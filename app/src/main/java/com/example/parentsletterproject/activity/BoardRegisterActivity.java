dbf02818cf10b000e9ca96eac79fd17303bd5cc7
baeseungwon <21storeo@gmail.com>
2022-06-06 14:21:36 +0900

    MODIFY board activities - seungwon

--- app/src/main/java/com/example/parentsletterproject/activity/BoardRegisterActivity.java
+++ app/src/main/java/com/example/parentsletterproject/activity/BoardRegisterActivity.java
@@ -1,16 +1,9 @@
 package com.example.parentsletterproject.activity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.parentsletterproject.R;
-
-import java.util.ArrayList;
 
 public class BoardRegisterActivity extends AppCompatActivity {
 
@@ -20,8 +13,6 @@ public class BoardRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_register);
-
-
 
 
     }
--- app/src/main/java/com/example/parentsletterproject/activity/MainActivity.java
+++ app/src/main/java/com/example/parentsletterproject/activity/MainActivity.java
@@ -3,25 +3,10 @@ package com.example.parentsletterproject.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 import com.example.parentsletterproject.R;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,13 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c0dadaab5ff14ca48e48aa850bb3652a&Type=xml&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7751096";
-        OpenAPI dust = new OpenAPI(url);
-        dust.execute();
+//        String url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c0dadaab5ff14ca48e48aa850bb3652a&Type=xml&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7751096";
+//        OpenAPI dust = new OpenAPI(url);
+//        dust.execute();
 
-        Button button = findViewById(R.id.button2);
+        Button button = findViewById(R.id.class_button);
         button.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), TeacherClassManagementActivity.class);
+            startActivity(intent);
+        });
+
+
+
+        Button button1 = findViewById(R.id.board_button);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), BoardListActivity.class);
             startActivity(intent);
         });
     }
