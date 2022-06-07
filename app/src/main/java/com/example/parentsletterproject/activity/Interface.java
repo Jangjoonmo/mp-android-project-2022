938b66115e0d0baf65b3af1db965808ecd57d82e
Suah-Cho <sacho0929@gmail.com>
2022-06-07 22:33:59 +0900

    msg

--- app/src/main/java/com/example/parentsletterproject/activity/Interface.java
+++
@@ -1,7 +0,0 @@
-package com.example.parentsletterproject.activity;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public interface Interface extends AppCompatActivity {
-
-}
--- app/src/main/java/com/example/parentsletterproject/activity/MainActivity.java
+++ app/src/main/java/com/example/parentsletterproject/activity/MainActivity.java
@@ -3,25 +3,10 @@ package com.example.parentsletterproject.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 import com.example.parentsletterproject.R;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,10 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        String url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c0dadaab5ff14ca48e48aa850bb3652a&Type=xml&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7751096";
-//        OpenAPI dust = new OpenAPI(url);
-//        dust.execute();
 
 
 
@@ -52,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
         Button button3 = findViewById(R.id.board_button);
         button3.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), BoardListActivity.class);
+            startActivity(intent);
+        });
+
+        Button button4 = findViewById(R.id.meal_button);
+        button4.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), MealActivity.class);
             startActivity(intent);
         });
     }
